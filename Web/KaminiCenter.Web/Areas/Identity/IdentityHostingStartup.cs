5c4317f6b224dfbd6c91113c27e3777c5e59096c
Николай Дойчев <nikolay.doichev@gmail.com>
2020-02-27 00:01:38 +0200

    Scafold Login and Register

---
+++ Web/KaminiCenter.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using KaminiCenter.Data;
+using KaminiCenter.Data.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(KaminiCenter.Web.Areas.Identity.IdentityHostingStartup))]
+namespace KaminiCenter.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
